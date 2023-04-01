f65a190e8f78a154f8d736379ef0cff70a9d37d8
Julius Caesar <juliuscaesarlaksana@gmail.com>
2023-04-01 15:10:35 +0700

    Putting backspace function to backspace button

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ inputAndResult.addEventListener("keyup", (event) => {
   if (event.key === "Enter") {
     const result = eval(inputAndResult.value);
     inputAndResult.textContent = result;
-    inputAndResult.value = result
+    inputAndResult.value = result;
     console.log(result);
   }
 });
+
+function clearInput() {
+  inputAndResult.value = "";
+}
+
+function backspaceInput() {
+  inputAndResult.value = inputAndResult.value.slice(0, -1);
+}
