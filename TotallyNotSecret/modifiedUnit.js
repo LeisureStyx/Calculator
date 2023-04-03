fa990ce52232449288b3e9737d57a363301e89ad
Julius Caesar <juliuscaesarlaksana@gmail.com>
2023-04-03 13:02:07 +0700

    Added unit.json to .gitignore due to knowledge limitation, and functional secret.html with classes and basic JSON

--- TotallyNotSecret/modifiedUnit.js
+++ TotallyNotSecret/modifiedUnit.js
@@ -8,10 +8,93 @@ class Units{
   }
 }
 
-const element = ["Fire", "Water", "Earth", "Thunder", "Light", "Dark"];
-const seria = new Units("Ruby Summoner", "Seria", 8589, element[0]);
-const vail = new Units("Glacial", "Vail &amp; Vidron", 8396, element[1]);
-const nia = new Units("Maverick of Elysia", "Nia", 8452, element[2]);
-const daltisk = new Units("True Revelation", "Daltisk", 1745, element[3]);
-const ionia = new Units("Metal Flower", "Ionia", 1588, element[4]);
-const shion = new Units("Disnomian Emperor", "Shion", 1590, element[5]);
+const input = document.getElementById("input");
+const unitJSON = `[
+  {    
+    "Title": "Ruby Summoner",    
+    "Name": "Seria",    
+    "Fullname": "Ruby Summoner Seria",    
+    "UnitNumber": 8589,    
+    "Element": "Fire"  
+  },  
+  {    
+    "Title": "Glacial",    
+    "Name": "Vail & Vidron",    
+    "Fullname": "Glacial Vail & Vidron",    
+    "UnitNumber": 8396,    
+    "Element": "Water"  
+  },  
+  {    
+    "Title": "Maverick of Elysia",    
+    "Name": "Nia",    
+    "Fullname": "Maverick of Elysia Nia",    
+    "UnitNumber": 8452,    
+    "Element": "Earth"  
+  },  
+  {    
+    "Title": "True Revelation",    
+    "Name": "Daltisk",    
+    "Fullname": "True Revelation Daltisk",    
+    "UnitNumber": 1745,    
+    "Element": "Thunder"  
+  },  
+  {    
+    "Title": "Metal Flower",    
+    "Name": "Ionia",    
+    "Fullname": "Metal Flower Ionia",    
+    "UnitNumber": 1588,    
+    "Element": "Light"  
+  },  
+  {    
+    "Title": "Disnomian Emperor",    
+    "Name": "Shion",    
+    "Fullname": "Disnomian Emperor Shion",    
+    "UnitNumber": 1590,    
+    "Element": "Dark"  
+  }
+]`;
+const units = JSON.parse(unitJSON);
+console.log(units)
+
+input.addEventListener("keypress", event => {
+  if(event.key === "Enter") {
+    const value = parseInt(event.target.value)
+    switch (value) {
+      case 1:
+        document.getElementById("unitName").innerHTML = units[0].Fullname;
+        document.getElementById("unitNumber").innerHTML = units[0].UnitNumber;
+        document.getElementById("unitElement").innerHTML = units[0].Element;
+        break;
+      case 2:
+        document.getElementById("unitName").innerHTML = units[1].Fullname;
+        document.getElementById("unitNumber").innerHTML = units[1].UnitNumber;
+        document.getElementById("unitElement").innerHTML = units[1].Element;
+        break;
+      case 3:
+        document.getElementById("unitName").innerHTML = units[2].Fullname;
+        document.getElementById("unitNumber").innerHTML = units[2].UnitNumber;
+        document.getElementById("unitElement").innerHTML = units[2].Element;
+        break;
+      case 4:
+        document.getElementById("unitName").innerHTML = units[3].Fullname;
+        document.getElementById("unitNumber").innerHTML = units[3].UnitNumber;
+        document.getElementById("unitElement").innerHTML = units[3].Element;
+        break;
+      case 5:
+        document.getElementById("unitName").innerHTML = units[4].Fullname;
+        document.getElementById("unitNumber").innerHTML = units[4].UnitNumber;
+        document.getElementById("unitElement").innerHTML = units[4].Element;
+        break;
+      case 6:
+        document.getElementById("unitName").innerHTML = units[5].Fullname;
+        document.getElementById("unitNumber").innerHTML = units[5].UnitNumber;
+        document.getElementById("unitElement").innerHTML = units[5].Element;
+        break;
+      default:
+        document.getElementById("unitName").innerHTML = "";
+        document.getElementById("unitNumber").innerHTML = "";
+        document.getElementById("unitElement").innerHTML = "";
+        break;
+    }
+  }
+})
